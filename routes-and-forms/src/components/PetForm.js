5edd8f792900fda45588845203971b6cf5b626f1
Rebecca Boateng <r.y.boateng@outlook.com>
2022-12-05 20:20:49 +0000

    form

--- routes-and-forms/src/components/PetForm.js
+++ routes-and-forms/src/components/PetForm.js
@@ -1,8 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const PetForm = () => {
-  const [name, setName] = useState('');
-  const [,setSubmitted] = useState(false);
+  const [name, setName] = useState("");
+  const [age, setAge] = useState(0);
+  const [colour, setColour] =  useState("");
+
+  const [, setSubmitted] = useState(false);
 
   function handleSubmit(event) {
     event.preventDefault();
@@ -15,16 +18,65 @@ const PetForm = () => {
       <h3>Please fill in your pet's details below</h3>
       <form onSubmit={(e) => handleSubmit(e)}>
         <div>
-          <label>Pet's Name:<input type="text" value={name} onChange={(e) => { setName(e.target.value); setSubmitted(false) }} /></label>
+          <label>
+            Pet's Name:
+            <input
+              type="text"
+              value={name}
+              onChange={(e) => {
+                setName(e.target.value);
+                setSubmitted(false);
+              }}
+            />
+          </label>
         </div>
-        <input type="submit" value="Submit" id="submit-button"/>
+
+
+        {/* age  */}
+        <div>
+          <label>
+            Pet's Age:
+            <input
+              type="number"
+              value={age}
+              onChange={(e) => {
+                setAge(e.target.value);
+                setSubmitted(false);
+              }}
+            />
+          </label>
+        </div>
+
+
+
+      {/* colour */}
+      <div>
+          <label>
+            Pet's Colour:
+            <input
+              type="text"
+              value={colour}
+              onChange={(e) => {
+                setColour(e.target.value);
+                setSubmitted(false);
+              }}
+            />
+          </label>
+        </div>
+
+
+        <input type="submit" value="Submit" id="submit-button" />
       </form>
       <div id="results">
         <h3>Results</h3>
-            Name: {name}<br />
+       <div>Name: {name} </div> 
+       <div>Age: {age}</div>
+       <div>Colour: {colour} </div>
+
+        <br />
       </div>
     </div>
   );
-}
+};
 
 export default PetForm;
--- routes-and-forms/src/index.js
+++ routes-and-forms/src/index.js
@@ -1,6 +1,7 @@
 import {React} from "react";
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router,Routes, Route, Link } from 'react-router-dom'
+import PetForm from "./components/PetForm";
 
 export default function App() {
   return (
@@ -8,10 +9,22 @@ export default function App() {
       <ul className="breadcrumb">
         <li><Link to="/"> All Pets </Link></li>
         <li><Link to="/kittens"> Kittens </Link></li>
+        <li><Link to="/puppies"> Puppies </Link></li>
+        <li><Link to="/ducklings"> Ducklings </Link></li>
+
       </ul>
       <Routes>
-        <Route path="/" element={<Home/>} />
+        {/* <Route path="/" element={<Home/>} />
         <Route path="kittens" element={<Kittens />} />
+        <Route path="puppies" element={<Puppies />} />
+        <Route path="ducklings" element={<Ducklings />} /> 
+   */}
+ <Route path="/" element={<Home/>} />
+        <Route path="kittens" element={<Animals name = {"kittens"} img = {"imgs/kitten.jpg"}  />}/>
+        <Route path="puppies" element={<Animals name = {"puppies"} img = {"imgs/puppy.jpg"}  />}/>
+        <Route path="ducklings" element={<Animals name = {"ducklings"} img= {"imgs/duckling.jpg"}  />} />
+  
+
       </Routes>
   </Router>
   );
@@ -21,18 +34,46 @@ function Home() {
   return <>
           <h1>All Pets</h1>
           <section>This is the page for all your pet needs</section>
+          <PetForm/>
           <img src="imgs/kitten.jpg" alt="Kitten"/>
           <img src="imgs/puppy.jpg" alt="Puppy"/>
           <img src="imgs/duckling.jpg" alt="Duckling"/>
         </>
 }
 
-function Kittens() {
-  return <>
-      <h1>Kittens</h1>
-      <section>This is the cats page</section>
-      <img src="imgs/kitten.jpg" alt="Kitten"/>
-    </>
+// function Kittens() {
+//   return <>
+//       <h1>Kittens</h1>
+//       <section>This is the cats page</section>
+//       <img src="imgs/kitten.jpg" alt="Kitten"/>
+//     </>
+// }
+
+// function Puppies () {
+//   return <>
+//   <h1>Puppies</h1>
+//   <section>This is the dogs page</section>
+//   <img src="imgs/puppy.jpg" alt="pup"/>
+// </>
+// }
+
+// function Ducklings () {
+//   return <>
+//   <h1>Ducklings</h1>
+//   <section>This is the ducklings page</section>
+//   <img src="imgs/duckling.jpg" alt="duck"/>
+// </>
+// }
+
+function Animals({name, img}) {
+  return (
+    <div>
+      <h2> {name} </h2>
+      <section> This is the {name} page</section>
+      <img src={img} alt={name}/> 
+
+    </div>
+  );
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
